02a457715d25d2c7f2089e1c387393f12e7346ca
Carlos Augusto <134291926+guto8888@users.noreply.github.com>
2023-12-19 22:00:26 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+         {
+           key: 'Content-Security-Policy',
+           value: "upgrade-insecure-requests"
+         }
+        ],
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
